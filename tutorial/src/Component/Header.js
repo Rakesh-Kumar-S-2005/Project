f51511039c51032d0e1b7e9017979b06bc4458bd
Rakesh-Kumar-S-2005 <230097.it@rmkec.ac.in>
2025-03-03 11:32:59 +0530

    Delete tutorial directory
    waste dir

--- tutorial/src/Component/Header.js
+++
@@ -1,6 +0,0 @@
-import "./Styleforlearn.css";
-export function Text() {
-    return<div className="title">
-     <h1 className="primary"></h1>
-     </div>
-}
--- tutorial/src/Component/test.js
+++
@@ -1,8 +0,0 @@
-let x=50
-if(x>=50)
-{
-    let x=500
-    console.log(x)
-}
-x=1000
-console.log(x)
